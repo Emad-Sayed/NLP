c2b3f4d3742ba36b536ce4e68405a9394081ee61
Emad <emad_tttt522@hotmail.com>
2018-05-01 00:59:50 +0200

    Fake Real Decision Tree

--- Fake_Real_Classification.py
+++ Fake_Real_Classification.py
@@ -39,3 +39,18 @@ def document_features(document):
 featuresets = [(document_features(d), c) for (d, c) in documents]
 train_set= featuresets[:3000]
 test_set = featuresets[3000:]
+classifier = nltk.NaiveBayesClassifier.train(train_set)
+print("NaiveBayesClassifier Accuracy     =>"+str(nltk.classify.accuracy(classifier, test_set)*100))
+classifier.show_most_informative_features(5)
+
+
+classifier = nltk.DecisionTreeClassifier.train(train_set,binary=False, entropy_cutoff=0.4, depth_cutoff=10, support_cutoff=20)
+print("DecisionTreeClassifier Accuracy     =>"+str(nltk.classify.accuracy(classifier, test_set)*100))
+# To Test This Application Put in File 1.txt and try to make the text large as possible because the features not large (small data set)
+#InputList=[]
+#with open("1.txt", 'r') as f:
+#    for line in f:
+#        for word in line.split():
+#            InputList.append(word)
+#            words.append(word)
+#print(classifier.classify(document_features(InputList)))
